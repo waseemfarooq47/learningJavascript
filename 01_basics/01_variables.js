7d5de983243f04efc814fcfdba8ffefac421026e
Waseem Farooq <49933034+waseemfarooq47@users.noreply.github.com>
2024-03-18 16:54:15 +0000

    variables and datatypes files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+//variables  are of 3 types
+/*
+    1. var
+    2. const
+    3.let
+*/
+
+
+// function conter(){
+//     for(var i=0; i<5;i++){
+//         console.log(i)
+//     }
+//     console.log(i)
+// }
+
+// conter()
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+// There are  primitive datatypes
+/*
+    1. number
+    2. bigInt
+    3. string
+    4. boolean
+    5. null
+    6. undefined
+    7. symbol
+*/
+let code = 12345
+console.log(typeof(code))
+
+let myName = 'waseem farooq'
+console.log(typeof(myName));
+
+let studentStatus=  true
+console.log(typeof(studentStatus));
+
+let initialFee = null
+console.log(typeof(initialFee));
+console.log(initialFee)
+
+let teacher;
+console.log(typeof(teacher));
+
+let sym = Symbol("hello")
+console.log(typeof(sym))
+console.log(sym)
+
+//non primitive datatypes
+/*
+    1. Arrays
+    2. Objects
+*/
+
+
+
+
