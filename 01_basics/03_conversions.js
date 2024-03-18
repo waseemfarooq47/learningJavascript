6bd2f1e37b36181038a340babf61ade09e306bd6
Waseem Farooq <49933034+waseemfarooq47@users.noreply.github.com>
2024-03-18 17:30:06 +0000

    conversions types

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,9 @@
+let myNumber = "aaa23"
+console.log(typeof myNumber) 
+// Requirement:: now i need to convert this into number we need coversion
+
+let changeIntoNumber = Boolean(myNumber)
+console.log(changeIntoNumber);
+console.log(typeof changeIntoNumber);
+
+
